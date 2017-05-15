6f8822a6866f9bc888c6d6a00aa3d4823291d08e
esulu <esulu1@ocdsb.ca>
2017-05-15 14:00:00 -0400

    Main Program
    This is the main program where the user enters an input and decides which string utility/utilities they wish to use.

---
+++ StringUtilities.java
@@ -0,0 +1,17 @@
+/*
+ ICS3U - Assignment String Utilities 
+ By Eren Sulutas, Aaron Buitenwurf, and Joshua Perkins 
+ 
+ Description:
+ Program that that allows the user to enter a single sentence or statement(s) and allows them to chose between various string utilities. 
+ 
+*/ 
+
+class StringUtilities
+{
+  public static void main(String[] args)
+  {
+    
+    
+  }
+}
